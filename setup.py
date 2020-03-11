e10d3ab81622745dd4df88f90559f8efc5632a35
Haotian Ren <allenren@Haotians-MacBook-Pro.local>
2020-03-11 10:07:04 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project used place api and geocode api to filter the closed stores',
+    author='Allen Ren',
+    license='',
+)
